149ce835d18f36a5cfcb0cb061f01428e772378f
HusamettinEEE <41583887+HusamettinEEE@users.noreply.github.com>
2019-10-31 20:18:07 +0300

    Update db_connection.js

--- db_connection.js
+++ db_connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv/config');
 
-mongoose.connect('mongodb+srv://husam:husamhusam@cluster0-s3a1g.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, () => {
+mongoose.connect('mongodb+srv://USER:PASSWORD@cluster0-s3a1g.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, () => {
   console.log('Db connected');
 });
